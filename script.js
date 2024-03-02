91caab4f0f3d85d8358e1669a81b2b5da8241a2e
HARSH-TRIPATHI-59 <mynameisharsh143@gmail.com>
2024-03-03 00:19:11 +0530

    Import and Export

---
+++ script.js
@@ -0,0 +1,10 @@
+
+var date = require('./script2')
+var a = 12;
+var b = 13;
+
+console.log(date)
+
+module.exports = {first: a, second: b}
+
+
---
+++ script2.js
@@ -0,0 +1,8 @@
+var data = require('./script')
+
+var c = 13;
+var d = 16;
+
+console.log(data)
+
+module.exports = {first: c, second: d}
